143de76777ae8c13fe88cd3693eeeb146f404ae2
bssthu <bssthu@users.noreply.github.com>
2015-11-04 20:22:08 +0800

    通过"服务器配置"的检查

--- server.js
+++ server.js
@@ -7,5 +7,24 @@
 // Description   : https://github.com/bssthu/wechat_listener
 // 
 
-var config = require('./config')
-console.log('The token is ' + config.token)
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var crypto = require('crypto');
+
+var config = require('./config');
+
+
+var server = http.createServer(function wechatReceiver(request, response) {
+  var query = querystring.parse(url.parse(request.url).query)
+
+  var arr = [ config.token, query.timestamp, query.nonce ];
+  var cryptoSrc = arr.sort().join('')
+  var sha1 = crypto.createHash('sha1');
+  var signature = sha1.update(cryptoSrc).digest('hex');
+
+  if (query.signature === signature) {
+    response.write(query.echostr);
+  }
+  response.end();
+}).listen(config.httpPort);
