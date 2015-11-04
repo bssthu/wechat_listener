b2cc8547ad84e517abaa41c8de13411fd32115fa
bssthu <bssthu@users.noreply.github.com>
2015-11-04 19:22:33 +0800

    read config file

---
+++ server.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// -*- coding:utf-8 -*-
+// File          : server.js
+// Author        : bss
+// Project       : wechat_listener
+// Creation Date : 2015-11-04
+// Description   : https://github.com/bssthu/wechat_listener
+// 
+
+var config = require('./config')
+console.log('The token is ' + config.token)
