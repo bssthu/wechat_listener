4bbaabc5ddb14f371f06cd1e66482d1b95a27dc6
bssthu <bssthu@users.noreply.github.com>
2015-11-04 20:57:17 +0800

    处理用户发送的普通消息

--- server.js
+++ server.js
@@ -6,16 +6,26 @@
 // Creation Date : 2015-11-04
 // Description   : https://github.com/bssthu/wechat_listener
 // 
+// Module dependencies:
+// - xml2js: https://github.com/Leonidas-from-XIV/node-xml2js
+// 
+
 
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 var crypto = require('crypto');
+var xml2js = require('xml2js');
 
 var config = require('./config');
 
 
-var server = http.createServer(function wechatReceiver(request, response) {
+var server = http
+  .createServer(wechatReceiver)
+  .listen(config.httpPort);
+
+
+function wechatReceiver(request, response) {
   var query = querystring.parse(url.parse(request.url).query)
 
   var arr = [ config.token, query.timestamp, query.nonce ];
@@ -23,8 +33,50 @@ var server = http.createServer(function wechatReceiver(request, response) {
   var sha1 = crypto.createHash('sha1');
   var signature = sha1.update(cryptoSrc).digest('hex');
 
+  // 身份验证，见“接入指南”
+  // http://mp.weixin.qq.com/wiki/17/2d4265491f12608cd170a95559800f2d.html
   if (query.signature === signature) {
-    response.write(query.echostr);
+    if (query.hasOwnProperty('echostr')) {
+      response.write(query.echostr);
+    }
   }
   response.end();
-}).listen(config.httpPort);
+
+  if (request.method === 'POST') {
+    request.setEncoding('utf-8');
+    var postData = '';
+
+    request.addListener('data', function (postDataChunk) {
+      postData += postDataChunk;
+    });
+
+    request.addListener('end', function () {
+      // 接收普通消息
+      // http://mp.weixin.qq.com/wiki/10/79502792eef98d6e0c6e1739da387346.html
+      handleXmlMessage(postData);
+    });
+  }
+}
+
+
+function handleXmlMessage(xmlMessage) {
+  xml2js.parseString(xmlMessage, function (err, result) {
+    if (!err) {
+      if (result.hasOwnProperty('xml')) {
+        xmlResult = result.xml;
+        if (xmlResult.hasOwnProperty('MsgType') &&
+            xmlResult.hasOwnProperty('Content')) {
+          if (xmlResult.MsgType[0] === 'text' &&
+              xmlResult.Content[0] !== null) {
+            handleTextMessage(xmlResult.Content[0]);
+          }
+        }
+      }
+    }
+  });
+}
+
+
+function handleTextMessage(text) {
+  // TODO: 转发
+}
